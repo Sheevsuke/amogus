f05e902e2d9f8fb88e248e7f0f99eeb020a1456c
clément bostyn <clemclem20002@gmail.com>
2022-10-19 10:13:23 +0200

    ajout final des tags + layer + comments du mouvement.cs

--- Assets/clement/scripts/mouvement.cs
+++ Assets/clement/scripts/mouvement.cs
@@ -7,20 +7,16 @@ public class mouvement : MonoBehaviour
     public Vector2 vec = new Vector2(0, 0);
     public float speed = 10;
     public float bonusSpeed = 1;
-    public float timeBonus = 5;
+    public float timeBonusMouvement = 5;
+    public float timeBonusPlayer = 5;
 
 
-    void Start()
-    {
-
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        //listen des touches zqsd et changement du vecteur unitaire
+        if (Input.GetKeyDown(KeyCode.Q)) 
             vec = new Vector2(-1, 0);
+
         if (Input.GetKeyDown(KeyCode.D))
             vec = new Vector2(1, 0);
 
@@ -36,35 +32,36 @@ public class mouvement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        // deplacement (tjr mieux en fixed update)
         this.transform.Translate(vec * Time.deltaTime * speed * bonusSpeed);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == 6)
+        if (collision.gameObject.layer == 6) //bonus de mouvement
         {
             bonusSpeed = 2;
-            StartCoroutine(bonus(timeBonus));
+            StartCoroutine(bonusMouvement(timeBonusMouvement));
             collision.gameObject.SetActive(false);
 
         }
-        else if (collision.gameObject.layer == 7)
+        else if (collision.gameObject.layer == 7) // bonus de changement de form (quand tu peux tuer les fantomes)
         {
             this.gameObject.tag = "playerTransform";
-            StartCoroutine(waiting(timeBonus));
+            StartCoroutine(waiting(timeBonusMouvement));
             collision.gameObject.SetActive(false);
         }
-        else if (collision.gameObject.layer == 8 && this.gameObject.tag == "playerTransform")
+        else if (collision.gameObject.layer == 8 && this.gameObject.tag == "playerTransform") // si on a le bonus de form et que tu touche un fantomes
         {
             collision.gameObject.SetActive(false);
         }
-        else if (collision.gameObject.layer == 8 && this.gameObject.tag == "Player")
+        else if (collision.gameObject.layer == 8 && this.gameObject.tag == "Player") // si on touche un fantome sans le bonus
         {
             //playerhit
         }
 
 
     }
-    IEnumerator bonus(float temps)
+    IEnumerator bonusMouvement(float temps) 
     {
 
         yield return new WaitForSeconds(temps);

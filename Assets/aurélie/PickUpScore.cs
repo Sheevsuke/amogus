a1723922c56008f7c13087f5d892783da671b380
shino <vshinori@gmail.com>
2022-10-20 16:49:47 +0200

    fix pick up score

--- Assets/aurélie/PickUpScore.cs
+++ Assets/aurélie/PickUpScore.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpPellet : MonoBehaviour
+public class PickUpScore : MonoBehaviour
 {
+    [SerializeField] private int addScore;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player"))
             return;
-        Score.instance.AddScore(10);
+        Score.instance.AddScore(addScore);
         // son collect
         Destroy(gameObject);
     }

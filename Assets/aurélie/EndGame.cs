ad9465fa27f8f44c4312fcf22b57aeb0112bc9f6
shino <vshinori@gmail.com>
2022-10-21 10:31:16 +0200

    win screen

--- Assets/aurélie/EndGame.cs
+++ Assets/aurélie/EndGame.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGame : MonoBehaviour
 {
     [SerializeField] private int _pelletLeft;
 
+    [SerializeField] private GameObject _winScreen;
+    [SerializeField] private Text _scoreCount;
+
+    [SerializeField] private Score _score;
+
     void Update()
     {
         _pelletLeft = transform.childCount;
         if (_pelletLeft == 0) // no pellet left > end game
+        {
             Time.timeScale = 0;
+            _winScreen.SetActive(true);
+            _scoreCount.text = _score.scoreCount.ToString();
+        }
     }
 }
